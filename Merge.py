59b6546375b853afd9a8958f52382132b674a449
EyadZaeim <75805994+EyadZaeim@users.noreply.github.com>
2021-12-10 14:45:49 +0300

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -1,18 +1,14 @@
 from __future__ import print_function
 import time
-import json
 
 from googleapiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-
 # Fill-in IDs of your Docs template & any Sheets data source
-DOCS_FILE_ID = '1pvE_0M19RBrbe-VSyAFULENd7IndWPe4CUjRpp5Fd24'
+DOCS_FILE_ID = '1rWK74DQA42K8VkbW3qPgyPFr8IhFZ4cUdq2z_XcmkiM'
 SHEETS_FILE_ID = input ('Please Enter The Sheet ID : ')
+Claimant_Name = input ('Please Enter The Claimant Name :')
 
 # authorization constants
 CLIENT_ID_FILE = 'credentials.json'
@@ -23,31 +19,12 @@ SCOPES = (  # iterable or space-delimited string
     'https://www.googleapis.com/auth/spreadsheets.readonly',
 )
 
-# It saves your credential data on mycreds.json FOR CONSTANT USE
-
-gauth = GoogleAuth()
-gauth.LoadCredentialsFile("mycreds.json")
-
-if gauth.credentials is None:
-    gauth.LocalWebserverAuth()
-
-elif gauth.access_token_expired:
-    gauth.Refresh()
-
-else:
-    gauth.Authorize()
-
-gauth.SaveCredentialsFile("mycreds.json")
-
 # application constants
 SOURCES = ('text', 'sheets')
 SOURCE = 'sheets' # Choose one of the data SOURCES
-COLUMNS = ['Owner_Name', 'Owner_Address', 'Type_of_Account', 'Amount', 'CoOwner', 'Securities', 'Property_ID']
+COLUMNS = ['Owner_Name', 'Owner_Address', 'Reported_By', 'Type_of_Account', 'Amount', 'CoOwner', 'Securities', 'Property_ID']
 TEXT_SOURCE_DATA = (
-    ('Hillel Sperling M.D.', '5620 Wilbur Ave #203 Tarzana Ca 91356', 'Associated Reproduction Services Inc.', '111 8th Ave\n'
-                                                'Vendor Check'),
-    ('Mr. Jeff Erson', 'Googler', 'Google NYC', '76 9th Ave\n'
-                                                'New York, NY  10011-4962'),
+ 
 )
 
 def get_http_client():
@@ -110,7 +87,7 @@ def merge_template(tmpl_id, source, service):
     # "search & replace" API requests for mail merge substitutions
     reqs = [{'replaceAllText': {
                 'containsText': {
-                    'text': '{{%s}}' % key.upper(), # {{VARS}} are uppercase
+                    'text': '{{%s}}' % key, # {{VARS}} are uppercase
                     'matchCase': True,
                 },
                 'replaceText': value,
@@ -125,29 +102,25 @@ def merge_template(tmpl_id, source, service):
 if __name__ == '__main__':
     # fill-in your data to merge into document template variables
     merge = {
-        # sender data
-        'my_name': 'Ayme A. Coder',
-        'my_address': '1600 Amphitheatre Pkwy\n'
-                      'Mountain View, CA  94043-1351',
-        'my_email': 'http://google.com',
-        'my_phone': '+1-650-253-0000',
         # - - - - - - - - - - - - - - - - - - - - - - - - - -
         # recipient data (supplied by 'text' or 'sheets' data source)
-        'to_name': None,
-        'to_title': None,
-        'to_company': None,
-        'to_address': None,
+        'Owner_Name': None,
+        'Owner_Address': None,
+        'Reported_By': None,
+        'Type_of_Account': None,
+        'Amount': None,
+        'CoOwner': None,
+        'Securities': None,
+        'Property_ID': None,
         # - - - - - - - - - - - - - - - - - - - - - - - - - -
-        'date': time.strftime('%Y %B %d'),
+
         # - - - - - - - - - - - - - - - - - - - - - - - - - -
-        'body': 'Google, headquartered in Mountain View, unveiled the new '
-                'Android phone at the Consumer Electronics Show. CEO Sundar '
-                'Pichai said in his keynote that users love their new phones.'
+      
     }
 
     # get row data, then loop through & process each form letter
     data = get_data(SOURCE) # get data from data source
-    for i, row in enumerate(data):
-        merge.update(dict(zip(COLUMNS, row)))
+    for i ,row in enumerate(data):
+        merge.update(dict(zip(COLUMNS,row)))
         print('Merged letter %d: docs.google.com/document/d/%s/edit' % (
-                i+1, merge_template(DOCS_FILE_ID, SOURCE, DRIVE)))
+            i + 1, merge_template(DOCS_FILE_ID, SOURCE, DRIVE)))
